b21644af50c4a13725dbceab7c98c3a61df343bd
ShankenduK <shankendu@gmail.com>
2025-02-28 02:16:59 +0530

    fixed bugs

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -76,7 +76,7 @@ const Login = () => {
   };
 
   return (
-    <div className="px-5 relative z-10  overflow-hidden md:px-0 w-full h-screen flex justify-center items-center dark:bg-linear-[180deg,#1B1F38_0%,#000000_100%] bg-linear-[180deg,#D1CAF0_0%,#F5F3FF_100%] before:w-24 md:before:w-40 before:h-24 md:before:h-40 before:absolute dark:before:bg-[#B882FF] before:bg-[#9F8BFF] before:rounded-full  before:blur-2xl before:top-[20%] before:left-[20%] after:w-32 md:after:w-44 after:h-32 md:after:h-44 after:absolute dark:after:bg-[#5DC1B9] after:bg-[#7FC8F8] after:rounded-full after:blur-2xl after:bottom-[20%] after:right-[20%]">
+    <div className="px-5 relative z-10  overflow-hidden md:px-0 w-full h-screen flex justify-center items-center dark:bg-linear-[180deg,#1B1F38_0%,#000000_100%] bg-linear-[180deg,#D1CAF0_0%,#F5F3FF_100%] before:w-24 md:before:w-40 before:h-24 md:before:h-40 before:absolute dark:before:bg-[#B882FF] before:bg-[#9F8BFF] before:rounded-full before:blur-2xl before:top-[20%] before:left-[20%] after:w-32 md:after:w-44 after:h-32 md:after:h-44 after:absolute dark:after:bg-[#5DC1B9] after:bg-[#7FC8F8] after:rounded-full after:blur-2xl after:bottom-[20%] after:right-[20%] after:-z-10">
       {/* Form Container */}
       <div className="p-10 w-full md:w-[50%] backdrop-blur-[12px] dark:bg-[rgba(0,0,0,0.1)] bg-[rgba(255,255,255,0.2)] border-[1.5px] dark:border-border-tertiary-dark border-border-tertiary-light [box-shadow:0_4px_10px_rgba(0,0,0,0.2)] rounded-xl flex flex-col justify-center items-center">
         {/* Login or signup header */}
@@ -183,7 +183,7 @@ const Login = () => {
             {form}
           </button>
           {/* Signup or login toggle */}
-          <p className="pt-2 text-xs sm:text-sm text-text-secondary-light dark:text-text-secondary-dark">
+          <p className="pt-2 text-xs sm:text-sm z-10 text-text-secondary-light dark:text-text-secondary-dark">
             {form === "LOGIN"
               ? "Don't have an account?"
               : "Already have an account?"}{" "}
